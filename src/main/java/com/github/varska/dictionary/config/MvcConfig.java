2e35cfd34b9dc27561f6c89756fbdfb35b9a9202
Evgeny Varshavsky <varshavskyevgeny@gmail.com>
2020-05-20 23:43:29 +0400

    Add Bootstrap

--- src/main/java/com/github/varska/dictionary/config/MvcConfig.java
+++ src/main/java/com/github/varska/dictionary/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.github.varska.dictionary.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,13 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("/webjars/")
+                .resourceChain(false)
+        ;
     }
 }
--- src/main/java/com/github/varska/dictionary/config/WebSecurityConfig.java
+++ src/main/java/com/github/varska/dictionary/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,6 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/registration", "/user/**", "/word/**").permitAll()
+                .antMatchers("/webjars/**").permitAll()
 //                .antMatchers("/admin/**").hasRole(Role.ADMIN.name())
                 .antMatchers("/add").hasRole(Role.USER.name())
                 .anyRequest().authenticated()
