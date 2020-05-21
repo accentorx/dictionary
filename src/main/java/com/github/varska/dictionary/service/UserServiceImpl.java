4c1f7244bf29d01ea36a6f15d05d3477c9a73b2d
Evgeny Varshavsky <varshavskyevgeny@gmail.com>
2020-05-21 14:15:51 +0400

    Fix empty space in username

--- src/main/java/com/github/varska/dictionary/service/UserServiceImpl.java
+++ src/main/java/com/github/varska/dictionary/service/UserServiceImpl.java
@@ -37,8 +37,9 @@ public class UserServiceImpl implements UserService{
     @Override
     public void save(User user) {
 
-        user.setRoles(Collections.singleton(Role.USER));
+        user.setUsername(user.getUsername().trim());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRoles(Collections.singleton(Role.USER));
 
         userRepo.save(user);
     }
